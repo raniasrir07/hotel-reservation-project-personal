45d58d825d61e990f3993ad8f7e8273877b02776
raniasrir-bit <rania.srir@uit.ac.ma>
2025-12-20 21:30:53 +0100

    Update Réservations.py

--- streamlit-app/pages/Réservations.py
+++ streamlit-app/pages/Réservations.py
@@ -83,7 +83,7 @@ st.set_page_config(
     layout="wide"
 )
 
-# Hide default Streamlit sidebar and nav
+# Hide defaulssst Streamlit sidebar and nav
 with open('styles/main.css') as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
